771eebc9a6548eb38cc15601e232e5d0d176103b
Tran <kiretran@mit.edu>
2019-06-10 12:19:11 +0900

    switched to a simpler script

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-console.log("init started");
-const WebSocket = require("ws");
+// console.log("init started");
+// const WebSocket = require("ws");
+//
+// const server = new WebSocket.Server({
+//     "host" : "ip-172-31-37-215.ap-northeast-1.compute.internal", // also tried public IP, but it throws an error
+//     "port" : 8080
+// });
+//
+// server.on("connection", function (client){
+//     console.log("client connection open");
+//
+//     client.on("message", function (data){
+//         console.log("client message received~! hi bitch <3");
+//     });
+// });
+//
+// server.on("listening", function (){
+//     console.log("server listening");
+// });
+//
+// server.on("error", function (e){
+//     console.log("server error", e);
+// });
+//
+// console.log("init done");
 
-const server = new WebSocket.Server({
-    "host" : "ip-172-31-37-215.ap-northeast-1.compute.internal", // also tried public IP, but it throws an error
-    "port" : 8080
-});
-
-server.on("connection", function (client){
-    console.log("client connection open");
-
-    client.on("message", function (data){
-        console.log("client message received~! hi bitch <3");
-    });
-});
-
-server.on("listening", function (){
-    console.log("server listening");
-});
-
-server.on("error", function (e){
-    console.log("server error", e);
-});
-
-console.log("init done");
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send('HEY!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
