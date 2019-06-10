0899afa349b0f2048fa12be7c251054611cc91f6
Tran <kiretran@mit.edu>
2019-06-10 12:28:26 +0900

    more changes

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-console.log("init started");
-const WebSocket = require("ws");
+// console.log("init started");
+// const WebSocket = require("ws");
+//
+// const server = new WebSocket.Server({
+//     "host" : "172.31.37.215", // private instance IP of 'perpperServer' AWS EC2
+//     "port" : 8080
+// });
+//
+// server.on("connection", function (client){
+//     console.log("client connection open");
+//
+//     client.on("message", function (data){
+//         console.log("client message received~! hi bitch <3");
+//     });
+// });
+//
+// server.on("listening", function (){
+//     console.log("server listening");
+// });
+//
+// server.on("error", function (e){
+//     console.log("server error", e);
+// });
+//
+// console.log("init done");
 
-const server = new WebSocket.Server({
-    "host" : "172.31.37.215", // private instance IP of 'perpperServer' AWS EC2
-    "port" : 8080
-});
-
-server.on("connection", function (client){
-    console.log("client connection open");
-
-    client.on("message", function (data){
-        console.log("client message received~! hi bitch <3");
-    });
-});
-
-server.on("listening", function (){
-    console.log("server listening");
-});
-
-server.on("error", function (e){
-    console.log("server error", e);
-});
-
-console.log("init done");
-
-// const express = require('express')
-// const app = express()
-// app.get('/', (req, res) => {
-//     res.send('HEY!')
-// })
-// app.listen(3000, () => console.log('Server running on port 3000'))
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send('HEY!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
