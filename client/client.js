a0fe4d5cc3f7112ef58140921df5fc834bd780e1
Tran <kiretran@mit.edu>
2019-06-19 16:11:19 +0900

    ??????

--- client/client.js
+++ client/client.js
@@ -104,23 +104,24 @@ const socket = new WebSocket('ws://ec2-3-16-66-225.us-east-2.compute.amazonaws.c
 
 // Connection opened
 socket.addEventListener('open', function (event) {
-    const loginMessage = new MicrobitLoginMessage();
-    loginMessage.setRoomName('room1');
-    loginMessage.setPassword('test1234');
-    loginMessage.setMicrobitName('kirererere');
-    let jsonMessage = loginMessage.build().toJson();
-    console.log('MESSAGE TO SEND FROM CLIENT: ' + jsonMessage);
-    socket.send(jsonMessage);
-
-    // const roboMessage = new RoboConnectorMessage();
-    // roboMessage.setRoomId(1);
-    // roboMessage.setUserId(129);
-    // roboMessage.setMessageType(messageType.handshake);
-    // roboMessage.setMessage('no message');
-    // roboMessage.setRobotId('');
-    // let jsonMessage = roboMessage.build().toJson();
+    // const loginMessage = new MicrobitLoginMessage();
+    // loginMessage.setRoomName('room1');
+    // loginMessage.setPassword('test1234');
+    // loginMessage.setMicrobitName('kirererere');
+    // let jsonMessage = loginMessage.build().toJson();
+    // console.log('MESSAGE TO SEND FROM CLIENT: ' + jsonMessage);
     // socket.send(jsonMessage);
-    // console.log('MESSAGE SENT FROM CLIENT: ' + jsonMessage);
+
+    const roboMessage = new RoboConnectorMessage();
+    roboMessage.setRoomId(1);
+    roboMessage.setUserId(129);
+    roboMessage.setMessageType(messageType.handshake);
+    roboMessage.setMessage('no message');
+    roboMessage.setRobotId('');
+    let jsonMessage = roboMessage.build().toJson();
+    socket.send(jsonMessage);
+    console.log('MESSAGE SENT FROM CLIENT: ' + jsonMessage);
+
 });
 
 // Listen for messages
